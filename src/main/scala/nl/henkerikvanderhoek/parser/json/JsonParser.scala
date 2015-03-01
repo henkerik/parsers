e50618c6e10e0d7a0beb4368aecd54151fd242c4
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2015-03-01 16:04:22 +0100

    Created lab session branch

--- src/main/scala/nl/henkerikvanderhoek/parser/json/JsonParser.scala
+++ src/main/scala/nl/henkerikvanderhoek/parser/json/JsonParser.scala
@@ -7,11 +7,7 @@ object JsonParser {
   val pJsonNull:Parser[JsonValue] =
     P.pSym("null").as(JsonNull())
 
-  val pJsonBoolean:Parser[JsonValue] = {
-    val pTrue  = P.pSym("true").as(JsonBoolean(true):JsonValue)
-    val pFalse = P.pSym("false").as(JsonBoolean(false):JsonValue)
-    pTrue choice pFalse
-  }
+  val pJsonBoolean:Parser[JsonValue] = ???
 
   val pJsonString:Parser[JsonValue] =
     for { str <- P.pToken(P.pString) } yield JsonString(str)
@@ -19,8 +15,7 @@ object JsonParser {
   val pJsonNumber:Parser[JsonValue] =
     for { n <- P.pToken(P.pDigits) } yield JsonNumber(n)
 
-  val pJsonArray:Parser[JsonValue] =
-    for { xs <- P.pBrackets(P.pSepBy(pJsonValue, P.pComma)) } yield JsonArray(xs)
+  val pJsonArray:Parser[JsonValue] = ???
 
   val pJsonObject:Parser[JsonValue] = {
     val pPair = for {
